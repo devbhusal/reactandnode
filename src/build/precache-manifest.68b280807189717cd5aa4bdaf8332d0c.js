8166ba40e7383de6d9342fc25cf538af734dc597
bhusal.dev1989 <bhusal.dev1989@gmail.com>
2020-09-23 16:52:37 +1000

    first

---
+++ src/build/precache-manifest.68b280807189717cd5aa4bdaf8332d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d148dd074d60d8ffe2d9a8404e637b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feb7c0dbdfff2da4b467",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "d3878bd133723b781681",
+    "url": "/static/js/2.d5f2f771.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5f2f771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb7c0dbdfff2da4b467",
+    "url": "/static/js/main.10c6fe26.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
